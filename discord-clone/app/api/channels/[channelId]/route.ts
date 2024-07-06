2f1ac26ae2058b5e8ba6e9f4f3a763e6b4492969
Devesh Patel <devesh1260@gmail.com>
2024-07-06 15:03:04 +0530

    edit and delete channels functionality completed

---
+++ discord-clone/app/api/channels/[channelId]/route.ts
@@ -0,0 +1,131 @@
+import { CurrentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+    req: Request,
+    { params } : {
+        params: {
+            channelId: string
+        }
+    }
+) {
+    try {
+        const profile = await CurrentProfile();
+
+        const { searchParams } = new URL(req.url)
+        const serverId = searchParams.get("serverId")
+
+        if(!profile){
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        if(!serverId){
+            return new NextResponse("Server missing", { status: 400 })
+        }
+
+        if(!params.channelId){
+            return new NextResponse("Channel does not exist", { status: 401 })
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                members: {
+                    some: {
+                        profileId: profile.id,
+                        role: {
+                            in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+                        }
+                    }
+                }
+            },
+            data: {
+                channel: {
+                    delete: {
+                        id: params.channelId,
+                        name: {
+                            not: "general"
+                        }
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(server)
+
+    } catch (error) {
+        console.log("[CHANNEL_DELETE_ERROR]", error);
+        return new NextResponse("Internal server error", { status: 500 })
+    }
+}
+
+export async function PATCH(
+    req: Request,
+    { params } : {
+        params : {
+            channelId: string
+        }
+    }
+) {
+    try{
+        const profile = await CurrentProfile();
+
+        const { searchParams } = new URL(req.url)
+        const { name, type } = await req.json()
+        const serverId = searchParams.get("serverId")
+
+        if(!profile){
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        if(!serverId){
+            return new NextResponse("Server missing", { status: 400 })
+        }
+
+        if(!params.channelId){
+            return new NextResponse("Channel missing", { status: 401 })
+        }
+
+        if(name === 'general'){
+            return new NextResponse("Cannnot edit general channel", { status: 401 })
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                members: {
+                    some: {
+                        profileId: profile.id,
+                        role: {
+                            in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+                        }
+                    }
+                }
+            },
+            data: {
+                channel: {
+                    update: {
+                        where: {
+                            id: params.channelId,
+                            name: {
+                                not: "general",
+                            }                    
+                        },
+                        data: {
+                            name: name,
+                            type: type,
+                        }
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(server)
+    } catch(error) {
+        console.log("[CHANNEL_EDIT_ERROR]", error);
+        return new NextResponse("Internal server error", { status: 500 })
+    }
+
+}   
--- discord-clone/components/modals/delete-channel-modal.tsx
+++ discord-clone/components/modals/delete-channel-modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import qs from "query-string"
+
 import {
   Dialog,
   DialogContent,
@@ -13,24 +15,32 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
-export const DeleteServerModal = () => {
-  const { isOpen, onClose, type, data } = useModal(); 
-  const isModalOpen = isOpen && type === "deleteServer"
+export const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const isModalOpen = isOpen && type === "deleteChannel"
 
-  const { server } = data;
+  const { server, channel } = data;
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const onClick = async () => {
     try {
       setIsLoading(true)
-      await axios.delete(`/api/servers/${server?.id}`)
+
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id
+        }
+      })
+
+      await axios.delete(url)
       
       onClose()
       router.refresh();
-
+      router.push(`/servers/${server?.id}`)
     } catch (error) {
       console.log(error);
 
@@ -44,10 +54,10 @@ export const DeleteServerModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Delete Server
+            Delete Channel
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Are you sure you want to delete <span className="font-semibold text-indigo-500">{server?.name}?</span>
+            Are you sure you want to delete <span className="font-semibold text-indigo-500">{channel?.name}?</span>
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
