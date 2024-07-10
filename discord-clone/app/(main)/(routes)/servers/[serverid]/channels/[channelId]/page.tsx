0a1d2ed453237c0b331c150c9c1bbb9700f42778
Devesh Patel <devesh1260@gmail.com>
2024-07-10 17:29:51 +0530

    chat message API done

--- discord-clone/app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
+++ discord-clone/app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
@@ -1,51 +1,60 @@
-import { ChatHeader } from "@/components/chat/chat-header"
-import { CurrentProfile } from "@/lib/current-profile"
-import { db } from "@/lib/db"
-import { redirectToSignIn } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
+import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import { CurrentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 interface ChannelIDPageProps {
-    params: {
-        channelId: string
-        serverId: string
-    }
+  params: {
+    channelId: string;
+    serverId: string;
+  };
 }
 
-const ChannelIDPage = async ({
-    params
-} : ChannelIDPageProps ) => {
-    const profile = await CurrentProfile();
+const ChannelIDPage = async ({ params }: ChannelIDPageProps) => {
+  const profile = await CurrentProfile();
 
-    if(!profile){
-        return redirectToSignIn();
-    }
+  if (!profile) {
+    return redirectToSignIn();
+  }
 
-    const channel = await db.channel.findUnique({
-        where: {
-            id: params.channelId
-        }
-    })
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
 
-    const member = await db.member.findFirst({
-        where: {
-            serverId: params.serverId,
-            profileId: profile.id
-        }
-    })
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
 
-    if(!channel || !member){
-        redirect("/")
-    }
+  if (!channel || !member) {
+    redirect("/");
+  }
 
-    return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-            <ChatHeader 
-                serverId={channel.serverId}
-                name={channel.name}
-                type="channel"
-             />
-        </div>
-    )
-}
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+            channelId: channel.id,
+            serverId: channel.serverId
+        }}
+      />
+    </div>
+  );
+};
 
 export default ChannelIDPage;
