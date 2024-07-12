b7d05f07b68dab58f82786bf5e46f67bc75cc169
Devesh Patel <devesh1260@gmail.com>
2024-07-12 17:29:41 +0530

    edit messages done

--- discord-clone/components/chat/chat-messages.tsx
+++ discord-clone/components/chat/chat-messages.tsx
@@ -1,95 +1,101 @@
-"use client"
+"use client";
 
-import { Member, Message, Profile } from "@prisma/client"
-import { ChatWelcome } from "./chat-welcome"
-import { useChatQuery } from "@/hooks/use-chat-query"
-import { Loader2, ServerCrash } from "lucide-react"
-import { Fragment } from "react"
+import { Member, Message, Profile } from "@prisma/client";
+import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
+import { Fragment } from "react";
+import { ChatItem } from "./chat-item";
+import { format } from "date-fns"
+
+const FORMAT = "d MMM yyyy, HH:MM"
 
 type MessageWithMemberWithProfile = Message & {
-    member: Member & {
-        profile: Profile
-    }
-}
+  member: Member & {
+    profile: Profile;
+  };
+};
 
 interface ChatMessagesProps {
-    name: string,
-    member: Member,
-    chatId: string,
-    apiUrl: string,
-    socketUrl: string,
-    socketQuery: Record<string, string>,
-    paramKey: "channelId" | "conversationId",
-    paramValue: string,
-    type: "channel" | "conversation"
+  name: string;
+  member: Member;
+  chatId: string;
+  apiUrl: string;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  type: "channel" | "conversation";
 }
 
 export const ChatMessages = ({
-    name,
-    member,
-    chatId,
-    apiUrl,
-    socketUrl,
-    socketQuery,
-    paramKey,
-    paramValue,
-    type
-} : ChatMessagesProps ) => {
-    const queryKey = `chat:${chatId}`
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
 
-    const {
-        data,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-        status
-    } = useChatQuery({
-        queryKey,
-        paramKey,
-        paramValue,
-        apiUrl
-    })
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({
+      queryKey,
+      paramKey,
+      paramValue,
+      apiUrl,
+    });
 
-    if(status === 'loading'){
-        return (
-            <div className="flex flex-col flex-1 justify-center items-center">
-                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
-                <p className="text-xs text-zinc-500 dark:text-zinc-400">
-                    Loading messages...
-                </p>
-            </div>
-        )
-    }
-
-    if(status === 'error'){
-        return (
-            <div className="flex flex-col flex-1 justify-center items-center">
-                <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
-                <p className="text-xs text-zinc-500 dark:text-zinc-400">
-                    Something went wrong!
-                </p>
-            </div>
-        )
-    }
-    
+  if (status === "loading") {
     return (
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-            <div className="flex-1" />
-            <ChatWelcome 
-                type={type}
-                name={name}
-            />          
-            <div className="flex flex-col-reverse mt-auto">
-                {data?.pages.map((group, i) => (
-                    <Fragment key={i}>
-                        {group.items.map((message: MessageWithMemberWithProfile) => (
-                            <div key={message.id}>
-                                {message.content}
-                            </div>
-                        ))}
-                    </Fragment>
-                ))}
-                </div>  
-        </div>
-    )
-}
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+      <div className="flex-1" />
+      <ChatWelcome type={type} name={name} />
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <ChatItem
+                key={message.id}
+                currentMember={member}
+                member={message.member}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                id={message.id}
+                content={message.content}
+                timestamp={format(new Date(message.createdAt), FORMAT)}
+                updated={message.createdAt !== message.updatedAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+               />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ discord-clone/pages/api/socket/messages/[messageId].ts
@@ -0,0 +1,157 @@
+import { CurrentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
+import { NextApiResponseServerIo } from "@/types";
+import { MemberRole } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponseServerIo
+) {
+    if(req.method !== "DELETE" && req.method !== "PATCH"){
+        return res.status(405).json({ error: "Method not allowed" })
+    }
+
+    try {
+        const profile = await CurrentProfilePages(req)
+        const { messageId, serverId, channelId } = req.query
+        const { content } = req.body
+
+        if(!profile){
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+
+        if(!messageId){
+            return res.status(400).json({ error: "Message ID missing" })
+        }
+        
+        if(!channelId){
+            return res.status(400).json({ error: "Channel ID missing" })
+        }
+
+        if(!content){
+            return res.status(400).json({ error: "Content missing" })
+        }
+
+        // console.log("Finding server with ID:", serverId);
+        const server = await db.server.findFirst({
+            where: {
+                id: serverId as string,
+                members: {
+                    some: {
+                        profileId: profile.id
+                    }
+                }
+            },
+            include: {
+                members: true
+            }
+        })
+
+        if(!server){
+            // console.log("Server not found");
+            return res.status(404).json({ error: "Server not found" })
+        }
+
+        // console.log("Finding channel with ID:", channelId);
+        const channel = await db.channel.findFirst({
+            where: {
+                id: channelId as string,
+                serverId: serverId as string
+            },
+        })
+
+        if(!channel){
+            // console.log("Channel not found");
+            return res.status(404).json({ error: "Channel not found" })
+        }
+
+        const member = server.members.find((member) => member.profileId === profile.id)
+
+        if(!member){
+            // console.log("Member not found");
+            return res.status(404).json({ error: "Member not found" })
+        }
+        
+        let message = await db.message.findFirst({
+            where: {
+                id: messageId as string,
+                channelId: channelId as string
+            },
+            include: {
+                member: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+        })
+
+        if(!message || message.deleted){
+            // console.log("Message not found or deleted");
+            return res.status(404).json({ error: "Message not found" })
+        }
+
+        const isMessageOwner = message.memberId === member.id
+        const isAdmin = member.role === MemberRole.ADMIN
+        const isModerator = member.role === MemberRole.MODERATOR
+        const canModify = isMessageOwner || isAdmin || isModerator
+
+        if(!canModify){
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+
+        if(req.method === "DELETE"){
+            message = await db.message.update({
+                where: {
+                    id: messageId as string,
+                    channelId: channelId as string
+                },
+                data: {
+                    fileUrl: null,
+                    content: "This message has been deleted",
+                    deleted: true
+                },
+                include: {
+                    member: {
+                        include: {
+                            profile: true
+                        }
+                    }
+                }
+            })
+        }
+
+        if(req.method === "PATCH"){
+            if(!isMessageOwner){
+                return res.status(401).json({ error: "Unauthorized" })
+            }
+
+            message = await db.message.update({
+                where: {
+                    id: messageId as string,
+                    channelId: channelId as string
+                },
+                data: {
+                    content
+                },
+                include: {
+                    member: {
+                        include: {
+                            profile: true
+                        }
+                    }
+                }
+            })
+        }
+
+        const updateKey = `chat:${channelId}:messages:update`
+
+        res?.socket?.server?.io?.emit(updateKey, message)
+
+        return res.status(200).json(message)
+    } catch (error) {
+        console.log("[MESSAGE_ID_ERROR]", error);
+        return res.status(500).json({ error: "Internal server error" })
+    }
+}
