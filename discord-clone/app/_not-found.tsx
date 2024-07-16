bacf43c5213fdc32c9dd63e3886751326a1f221b
Devesh Patel <devesh1260@gmail.com>
2024-07-16 11:57:08 +0530

    commit

---
+++ discord-clone/app/_not-found.tsx
@@ -0,0 +1,9 @@
+export const runtime = "edge";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+    </div>
+  );
+}
---
+++ discord-clone/app/index.tsx
@@ -0,0 +1,9 @@
+export const runtime = "edge";
+
+export default function Index() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+    </div>
+  );
+}
